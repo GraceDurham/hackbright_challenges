9a52e1e1aa67299ee0b0b5acd3713594b6f7fb88
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2018-03-26 09:37:09 -0700

    add concatenate_list function

---
+++ concatenate_list.py
@@ -0,0 +1,7 @@
+#Given two lists. concatenate them (that is, combine them into a single list).
+def con_cate(lst1, lst2):
+    for num in lst2:
+        lst1.append(num)
+
+    return lst1
+print con_cate([1,2],[3,4])
